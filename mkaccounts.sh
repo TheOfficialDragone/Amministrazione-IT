4c4c114466757bc7349c97d641abbfec20377f56
TheOfficialDragone <rocco.carpi122@gmail.com>
2021-10-18 14:13:45 +0200

    Create mkaccounts.sh

---
+++ mkaccounts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#controllare se un utente esiste e mostrare un messaggio di creazione account con successso
+
+#variabili 
+NEWUSERFILE =  /tmp/support/newusers
+
+case $OPTION in 
+
+    '')
+        ;;
+
+    -v)
+        VERBOSE = y;
+        ;;
+    
+    -h)
+        echo "Usage : $0 [-h|-v]"
+        echo
+        exit
+        ;;
+    
+    *)
+        echo "Usage : $0 [-h|-v]"
+        echo
+        exit 1  #messaggio di errore 
+        ;;
+
+esac
